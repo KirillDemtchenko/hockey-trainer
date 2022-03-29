b83672aa84340c812e08af6fc7a6a67e42b199e4
Kirill Demchenko <kirill.demchenko@x5.ru>
2022-03-29 14:41:43 +0300

    Added cloud functions terraform manifest

---
+++ infrastructure/cloud-functions.tf
@@ -0,0 +1,14 @@
+resource "yandex_function" "test-function" {
+  name               = "some_name"
+  description        = "any description"
+  user_hash          = "any_user_defined_string"
+  runtime            = "python37"
+  entrypoint         = "main"
+  memory             = "128"
+  execution_timeout  = "10"
+  service_account_id = "are1service2account3id"
+  tags               = ["my_tag"]
+  content {
+    zip_filename = "function.zip"
+  }
+}
